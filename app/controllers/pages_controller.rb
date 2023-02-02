3e017c7918eee08b2232c859c0f65da3f017bde5
Alex Vasse <alexandervasse@gmail.com>
2023-02-02 14:03:05 +0900

    css adjustments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  before_action :redirect_user, only: :home
+
   def home
+  end
+
+  private
+
+  def redirect_user
+    redirect_to rooms_path if current_user
   end
 end
