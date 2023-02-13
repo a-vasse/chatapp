c3e031eacdc393cc7151e1b34c7336db2d2b7453
Alex Vasse <alexandervasse@gmail.com>
2023-02-13 16:09:01 +0900

    changed domnode to mutationobserver

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -2,9 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+
+    //Replaces old "DomNodeInserted" with "MutationObserver"
+    //Created MutationObserver object and passed the resetScroll function as its callback.
+    //Called the observe method on the observer and pass messages as its target.
+    //This sets up the observer to observe the messages element for any changes to its child nodes.
     console.log("Connected");
     const messages = document.getElementById("messages");
-    messages.addEventListener("DomNodeInserted", this.resetScroll);
+    const observer = new MutationObserver(this.resetScroll);
+    observer.observe(messages, { childList: true });
     this.resetScroll(messages);
   }
 
@@ -13,7 +19,6 @@ export default class extends Controller {
   }
 
   resetScroll() {
-    console.log("Reset Scroll Action");
     messages.scrollTop = messages.scrollHeight - messages.clientHeight;
   }
 }
