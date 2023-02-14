88a601583732fc5f3b5c5501e6b5e2973d14cc18
Alex Vasse <alexandervasse@gmail.com>
2023-02-15 02:11:07 +0900

    attachments implemented, not yet working

--- app/javascript/controllers/message_preview_controller.js
+++ app/javascript/controllers/message_preview_controller.js
@@ -1,11 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
-/* controller respoinsible for displaying preview messages */
+/**
+ * This controller is responsible for displaying the message preview.
+ * @class MessagePreviewController
+ */
 
 export default class extends Controller {
-  connect() {}
-
-  /* creates the preview panel displayed above the message input */
+  connect() {
+    console.log("message preview controller connected")
+  }
+  /**
+   * Creates the preview panel displayed above the message input.
+   */
   preview() {
     this.clearPreviews();
     for (let i = 0; i < this.targets.element.files.length; i++) {
@@ -15,7 +21,12 @@ export default class extends Controller {
     }
   }
 
-  /* creates and displays preview elements */
+  /**
+   * Creates and displays the preview elements for the file.
+   *
+   * @param {*} file - The file to be previewed
+   * @param {*} reader - The FileReader object
+   */
   createDisplayPreviewElements(file, reader) {
     reader.onload = () => {
       let element = this.constructPreviews(file, reader);
@@ -28,7 +39,14 @@ export default class extends Controller {
     reader.readAsDataURL(file);
   }
 
-  /* construct the preview element for the file */
+  /**
+   * Constructs the preview element for the file.
+   * These elements are used to display the file in the message preview.
+   *
+   * @param {*} file - The file to be previewed
+   * @param {*} reader - The FileReader object
+   * @returns {HTMLElement} - The element to be added to the DOM
+   */
   constructPreviews(file, reader) {
     let element;
     let cancelFunction = (e) => this.removePreview(e);
@@ -39,22 +57,27 @@ export default class extends Controller {
         element = this.createImageElement(cancelFunction, reader);
         break;
       case "video/mp4":
-      case "video/quicktime":
-        element = this.createVideoElement(cancelFunction, reader);
-        break;
+        case "video/quicktime":
+          element = this.createVideoElement(cancelFunction);
+          break;
       case "audio/mpeg":
       case "audio/mp3":
       case "audio/wav":
-        element = this.createAudioElement(cancelFunction, reader);
+        element = this.createAudioElement(cancelFunction);
         break;
       default:
-        element = this.createDefaultElement(cancelFunction, reader);
-    }
-    element.dataset.filename = file.name;
-    return element;
+        element = this.createDefaultElement(cancelFunction);
+      }
+      element.dataset.filename = file.name;
+      return element;
   }
 
-  /* creates an image preview element */
+  /**
+   * Create an image preview element.
+   * @param {*} cancelFunction - The function to be called when the cancel button is clicked
+   * @param {*} reader - The FileReader object
+   * @returns {HTMLElement} - The element to be added to the DOM
+   */
   createImageElement(cancelFunction, reader) {
     let cancelUploadButton, element;
     const image = document.createElement("img");
@@ -64,47 +87,66 @@ export default class extends Controller {
     element.classList.add("attachment-image-container", "file-removal");
     element.appendChild(image);
     cancelUploadButton = document.createElement("i");
-    cancelUploadButton.classList.add("cancel-upload-button");
+    cancelUploadButton.classList.add("bi","bi-x-circle-fill","cancel-upload-button");
     cancelUploadButton.onclick = cancelFunction;
     element.appendChild(cancelUploadButton);
     return element;
   }
 
-  /* creates an audio preview element */
+  /**
+   * Creates a preview element for audio files.
+   * This is used for audio files that are of type: mp3, or wav.
+   * @param {*} cancelFunction - The function to be called when the cancel button is clicked
+   * @returns {HTMLElement} - The element to be added to the DOM
+   */
   createAudioElement(cancelFunction) {
     let cancelUploadButton, element;
     element = document.createElement("i");
-    element.classList.add("audio-preview-icon", "file-removal");
+    element.classList.add("bi", "bi-file-earmark-music-fill", "audio-preview-icon", "file-removal");
     cancelUploadButton = document.createElement("i");
-    cancelUploadButton.classList.add("cancel-upload-button");
+    cancelUploadButton.classList.add("bi", "bi-x-circle-fill", "cancel-upload-button");
     cancelUploadButton.onclick = cancelFunction;
     element.appendChild(cancelUploadButton);
     return element;
   }
 
+  /**
+   * Creates a video preview element.
+   * @param {*} cancelFunction - The function to be called when the cancel button is clicked
+   * @returns {HTMLElement} - The element to be added to the DOM
+   */
   createVideoElement(cancelFunction) {
     let cancelUploadButton, element;
     element = document.createElement("i");
-    element.classList.add("video-preview-icon", "file-removal");
+    element.classList.add("bi", "bi-file-earmark-play-fill", "video-preview-icon", "file-removal");
     cancelUploadButton = document.createElement("i");
-    cancelUploadButton.classList.add("cancel-upload-button");
+    cancelUploadButton.classList.add( "bi", "bi-x-circle-fill", "cancel-upload-button");
     cancelUploadButton.onclick = cancelFunction;
     element.appendChild(cancelUploadButton);
     return element;
   }
-
+  /**
+   * Creates the element for a default file type. This is used for files that
+   * are (probably) not images, videos, or audio.
+   * @param {*} cancelFunction - The function to be called when the cancel button is clicked
+   * @returns {HTMLElement} - The element to be added to the DOM
+   */
   createDefaultElement(cancelFunction) {
     let cancelUploadButton, element;
     element = document.createElement("i");
-    element.classList.add("file-preview-icon", "file-removal");
+    element.classList.add("bi", "bi-file-check-fill", "file-preview-icon", "file-removal");
     cancelUploadButton = document.createElement("i");
-    cancelUploadButton.classList.add("cancel-upload-button");
+    cancelUploadButton.classList.add( "bi", "bi-x-circle-fill", "cancel-upload-button");
     cancelUploadButton.onclick = cancelFunction;
     element.appendChild(cancelUploadButton);
     return element;
   }
 
-  /* remove selected preview element */
+  /**
+   * Remove the selected preview element.
+   * Uses a dataTransfer to circumvent fileList limitations
+   * @param {Event} e - The event object
+   */
   removePreview(event) {
     const target = event.target.parentNode.closest(".attachment-preview");
     const dataTransfer = new DataTransfer();
@@ -121,7 +163,9 @@ export default class extends Controller {
     fileInput.files = dataTransfer.files;
   }
 
-  /* clear all preview elements after submit */
+  /**
+   * Clear all the preview elements after submit
+   */
   clearPreviews() {
     document.getElementById("attachment-previews").innerHTML = "";
   }
