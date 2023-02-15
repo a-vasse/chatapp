0228913891575432a956cc0a0cac5885ad814a95
Alex Vasse <alexandervasse@gmail.com>
2023-02-15 15:06:14 +0900

    well that fixed it somehow

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,26 +1,27 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_status
-
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
     render 'index'
   end
 
   def show
     @single_room = Room.find(params[:id])
+
     @room = Room.new
     @rooms = Room.public_rooms
-    @users = User.all_except(current_user)
+
     @message = Message.new
 
-    # @messages = @single_room.messages.order(created_at: :asc)
-    pagy_messages = @single_room.messages.order(created_at: :asc)
-    @pagy, messages = pagy(pagy_messages, items: 20)
+    pagy_messages = @single_room.messages.order(created_at: :desc)
+    @pagy, messages = pagy(pagy_messages, items: 10)
     @messages = messages.reverse
-    
+
+    @users = User.all_except(current_user)
     render 'index'
   end
 
